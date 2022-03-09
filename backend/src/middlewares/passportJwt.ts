0dcfaef0e03b38809a028969578f4accfe3dc066
Ritesh Khore <riteshkhore@gmail.com>
2022-03-09 16:05:13 +0530

    Backend commit

---
+++ backend/src/middlewares/passportJwt.ts
@@ -0,0 +1,35 @@
+import { Request } from "express";
+import { PassportStatic } from "passport";
+import { Strategy as JwtStrategy } from "passport-jwt";
+import { ACESS_TOKEN_SECRET } from "../keys/secrets";
+import User from "../model/userModel";
+
+const extractTokenFromCookie = (req: Request) => {
+  const { accessToken } = req.cookies;
+
+  return accessToken;
+};
+
+export const passportJwt = (passport: PassportStatic) => {
+  passport.use(
+    new JwtStrategy(
+      {
+        jwtFromRequest: extractTokenFromCookie,
+        secretOrKey: ACESS_TOKEN_SECRET,
+      },
+      async (payload, done) => {
+        const _id = payload._id;
+
+        try {
+          const user = await User.findOne({ _id });
+
+          if (!user) return done(null, false);
+
+          done(null, user);
+        } catch (error) {
+          done(error, false);
+        }
+      }
+    )
+  );
+};
